ae6853e94f5212a1e5ac5dcc90dea1d0be78e86f
Priti Solanki <pritisolanki@ME-13-NTB.kontent.ai>
2022-12-19 17:37:00 -0500

    Task: Add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,18 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/bafta-headshot.Jpeg"
+      />
+      <p> Sample Image is taken from Google Image</p>
     </Layout>
   )
 }
